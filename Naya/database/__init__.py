151025a5d6233065428241bee2d61094c077ba6b
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-21 00:55:41 +0700

    Update __init__.py

--- Naya/database/__init__.py
+++ Naya/database/__init__.py
@@ -1,17 +1,3 @@
 from env import DATABASE_URL
 
-count_ = 0
-def start() -> scoped_session:
-    if DATABASE_URL == "":
-        if count_ < 1:
-            count += 1
-            return print("Database url not provided..\nBut this time I won't stop 😉")
-        return
-    engine = create_engine(DATABASE_URL)
-    BASE.metadata.bind = engine
-    BASE.metadata.create_all(engine)
-    return scoped_session(sessionmaker(bind=engine, autoflush=False))
 
-if DATABASE_URL != "":
-    BASE = declarative_base()
-    SESSION = start()
