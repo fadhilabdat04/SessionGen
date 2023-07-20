8a04cd4e8c9b671643b9b5e8d2f7671072bb5cb3
fadhilabdat04 <98678420+fadhilabdat04@users.noreply.github.com>
2023-07-21 00:53:54 +0700

    Update __init__.py

--- Naya/database/__init__.py
+++ Naya/database/__init__.py
@@ -1,7 +1,3 @@
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, scoped_session
-
 from env import DATABASE_URL
 
 count_ = 0
